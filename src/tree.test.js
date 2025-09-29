cb799a3a62b376f1c33ba31cb5761a8706d1082e
a.y.kuznetsov <a.y.kuznetsov@fun-box.ru>
2025-09-29 23:25:28 +0300

    Добавление недостающих тестов вывода дерева

--- src/tree.test.js
+++ src/tree.test.js
@@ -1,23 +1,23 @@
 const { printTree } = require('./tree');
 
-const nestedTreeData = {
-  'name': 1,
-  'items': [
+const nestedTree = {
+  name: 1,
+  items: [
     {
-      'name': 2,
-      'items': [
-        { 'name': 3 },
-        { 'name': 4 },
+      name: 2,
+      items: [
+        { name: 3 },
+        { name: 4 },
       ],
     },
     {
-      'name': 5,
-      'items': [
-        { 'name': 6 },
+      name: 5,
+      items: [
+        { name: 6 },
       ]
     },
   ]
-}
+};
 
 const nestedTreeLines = [
   '└── 1',
@@ -28,14 +28,88 @@ const nestedTreeLines = [
   '  └── 6',
 ];
 
+const wideTree = {
+  name: 1,
+  items: [
+    {
+      name: 2,
+      items: [
+        { name: 7 },
+        { name: 8 },
+        { name: 9 },
+        { name: 10 },
+      ]
+    },
+    { name: 3 },
+    { name: 4 },
+    { name: 5 },
+    { name: 6 },
+  ]
+};
+
+const wideTreeLines = [
+  '└── 1',
+  ' ├── 2',
+  ' │ ├── 7',
+  ' │ ├── 8',
+  ' │ ├── 9',
+  ' │ └── 10',
+  ' ├── 3',
+  ' ├── 4',
+  ' ├── 5',
+  ' └── 6',
+];
+
+const deepTree = {
+  name: 1,
+  items: [{
+    name: 2,
+    items: [{
+      name: 3,
+      items: [{
+        name: 4,
+        items: [
+          {
+            name: 5,
+            items: [{ name: 'a' }],
+          },
+          {
+            name: 6,
+            items: [{ name: 'b' }],
+          },
+        ],
+      }],
+    }],
+  }],
+};
+
+const deepTreeLines = [
+  '└── 1',
+  ' └── 2',
+  '  └── 3',
+  '   └── 4',
+  '    ├── 5',
+  '    │ └── a',
+  '    └── 6',
+  '     └── b',
+]
+
 describe('Утилита tree', () => {
-  test('Выводит корректный результат для дерева со вложенными узлами в консоль', () => {
-    const logSpy = jest.spyOn(console, 'log');
+  [
+    { value: nestedTree, expected: nestedTreeLines, description: 'дерева со вложенными узлами' },
+    { value: {}, expected: [], description: 'пустого объекта' },
+    { value: { name: 1 }, expected: ['└── 1'], description: 'дерева с одиночным узлом' },
+    { value: wideTree, expected: wideTreeLines, description: 'дерева с большой шириной' },
+    { value: deepTree, expected: deepTreeLines, description: 'дерева с большой шириной' },
+  ].forEach(({ value, expected, description }) => {
+    test(`Выводит корректный результат для ${description} в консоль`, () => {
+      console.log = jest.fn();
 
-    printTree(nestedTreeData)
+      printTree(value)
 
-    expect(logSpy).toHaveBeenCalledWith(
-      nestedTreeLines.join('\n')
-    );
+      expect(console.log).toHaveBeenCalledWith(
+        expected.join('\n')
+      );
+    });
   });
 })
